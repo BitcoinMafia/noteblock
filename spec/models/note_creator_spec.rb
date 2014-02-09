108b26e3da591dc00a7fb517f8c6f904acb4284d
Scott Li <scottli0101@gmail.com>
2014-02-09 15:29:51 +1100

    failing for payment/creator

--- spec/models/note_creator_spec.rb
+++ spec/models/note_creator_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe NoteCreator do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    let(:valid_hash) { "6f9e9570881e781db8c137c84c111a138e4a022e6b2def5e2a1589a802fe25f3" }
+    let(:invalid_hash) { "this is obviously not correct" }
+
+    before(:each) do
+      @note_creator = NoteCreator.new
+    end
+
+    it "should not save without tx_hash" do
+      @note_creator.tx_hash = valid_hash
+      expect(@note_creator.save).to eq(false)
+    end
+
+    it "should not save with an invalid tx_hash" do
+      @note_creator.tx_hash = invalid_hash
+      expect(@note_creator.save).to eq(false)
+    end
+
+    it "should otherwise save" do
+      @note_creator.tx_hash = valid_hash
+      expect(@note_creator.save).to eq(true)
+    end
+  end
 end
--- spec/models/note_payment_spec.rb
+++ spec/models/note_payment_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe NotePayment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    let(:valid_hash) { "6f9e9570881e781db8c137c84c111a138e4a022e6b2def5e2a1589a802fe25f3" }
+    let(:invalid_hash) { "this is obviously not correct" }
+
+    before(:each) do
+      @note_payment = NotePayment.new
+    end
+
+    it "should not save without tx_hash" do
+      @note_payment.tx_hash = valid_hash
+      expect(@note_payment.save).to eq(false)
+    end
+
+    it "should not save with an invalid tx_hash" do
+      @note_payment.tx_hash = invalid_hash
+      expect(@note_payment.save).to eq(false)
+    end
+
+    it "should otherwise save" do
+      @note_payment.tx_hash = valid_hash
+      expect(@note_payment.save).to eq(true)
+    end
+  end
 end
