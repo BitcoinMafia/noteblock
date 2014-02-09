9e3013f0d581300787259f2b06ead2fe219d5c21
Scott Li <scottli0101@gmail.com>
2014-02-09 15:40:06 +1100

    note validations passing

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,18 @@
 class Note < ActiveRecord::Base
+  validates :content, presence: true, length: { maximum: 76 }
+  validates :address, presence: true
+  validates :encrypted_private_key, presence: true
+  validate :private_key_encrypted
+
+  def private_key_encrypted
+
+    # Check if encrypted_private_key all valid hex characters
+    # This implies it has not been AES encrypted
+    if encrypted_private_key && !encrypted_private_key[/\H/]
+      errors.add(:note_id, "Cannot store unencrypted private key")
+      return false
+    end
+  end
+
+
 end
