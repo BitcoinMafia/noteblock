a67e409393e30cb499fea55e867bef61377c2264
Scott Li <scottli0101@gmail.com>
2014-02-14 05:24:44 +1100

    added sweeper backup

---
+++ app/services/note_sweeper.rb
@@ -0,0 +1,36 @@
+module NoteSweeper
+  extend self
+
+  def check
+    notes = Note.includes(:note_transactions).where(
+      "note_transactions.tx_type = 'payment' OR note_transactions.tx_type = 'proof'")
+    not_propagated = notes.map do |note|
+      note if note.note_transactions.proofs.blank?
+    end
+
+    ap "THE FOLLOWING HAVE RECEIVED PAYMENT, BUT HAVE NOT PROPAGATED/EMBEDDED MESSAGE"
+    return not_propagated.compact
+  end
+
+  def finish_task_for_note(note_id)
+    note = Note.where(id: note_id)[0]
+
+    # # NoteTransaction
+    if !NoteRunner::Task.create_proof(note, compressed: false);
+      ap "Note Proof Failed"
+      return false
+    end
+
+    # # Token
+    if !NoteRunner::Task.create_token(note);
+      ap "Note Proof Failed"
+      return false
+    end
+
+    # # Email
+    if !NoteRunner::Task.send_email(note);
+      ap "Emailed Failed"
+      return false
+    end
+  end
+end
